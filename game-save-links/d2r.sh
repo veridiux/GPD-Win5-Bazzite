0f1897a01576a3388719283bb5901b31c2387ce1
justin <veridiux@gmail.com>
2026-08-26 03:53:56 -0500

    update: 2026-08-26 03:53:56

--- game-save-links/d2r.sh
+++ game-save-links/d2r.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/.local/share/Steam/steamapps/compatdata/2536520/pfx/drive_c/users/steamuser/Saved\ Games
+
 ln -s ~/Sync/game_saves/Diablo\ II\ Resurrected/ \
   ~/.local/share/Steam/steamapps/compatdata/2536520/pfx/drive_c/users/steamuser/Saved\ Games/Diablo\ II\ Resurrected
